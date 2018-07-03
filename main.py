e268486e9359ea478094450121bdc82a8ec6043b
corenel <xxdsox@gmail.com>
2018-07-03 15:37:57 +0800

    add argparse

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from selenium import webdriver
-import util
-import setting
+
+import argparse
 import json
 import os
 
+import util
+import setting
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Spider for Crypko')
+    parser.add_argument('--from', '-f', type=int, default=1, help='Start id')
+    parser.add_argument('--to', '-t', type=int, default=setting.CRYPKO_MAX_ID, help='Stop id')
+    args = vars(parser.parse_args())
+
     browser = webdriver.Chrome()
     if not os.path.exists(setting.SAVE_DIR):
         os.makedirs(setting.SAVE_DIR)
 
-    for crypko_id in range(1, setting.CRYPKO_MAX_ID + 1):
+    for crypko_id in range(args['from'], args['to'] + 1):
         print('====== {} ======='.format(crypko_id))
 
         if not os.path.exists(setting.SAVE_FILENAME.format(crypko_id)):
